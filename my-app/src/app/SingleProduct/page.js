d79761045934e77993a0f1c54f30eb8b0ccbbd00
niroj satyal <niroj.satyal2000@gmail.com>
2023-07-21 00:25:25 -0700

    install next-app

---
+++ my-app/src/app/SingleProduct/page.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const page = () => {
+  return (
+    <div>single product</div>
+  )
+}
+
+export default page
---
+++ my-app/src/app/components/Header.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-max[80%] mx-auto flex  justify-between'>
+        <h1 className='text-bold text-white '>OnlineStore</h1>
+        <div>
+           <h2>other product</h2>
+        </div>
+
+       
+    </div>
+  )
+}
+
+export default Header
